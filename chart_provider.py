bf09030a6506f74cce18e9bb8a2d37f878f083ac
Michael Davey <mikey@new-signal.com>
2024-01-15 21:50:16 +0000

    Slight adjustment to colour bands

--- chart_provider.py
+++ chart_provider.py
@@ -22,9 +22,10 @@ class ChartProvider:
             df, y="value_inc_vat", x="valid_from",
             color="value_inc_vat",
             color_continuous_scale=[
-                (0, "chartreuse"), (0.1, "chartreuse"),
+                (0, "chartreuse"), (0.05, "chartreuse"),
+                (0.05, "seagreen"), (0.1, "seagreen"),
                 (0.1, "orange"), (0.8, "orange"),
-                (0.8, "crimson"), (1, "crimson")
+                (0.8, "crimson"), (1, "crimson"),
             ],
             width=1024, height=768,
         )
